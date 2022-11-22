5ef5681281e1952ff2c8fdde993cee9b2338407a
Pedro <pedrovenegasjob@gmail.com>
2022-11-22 16:01:50 -0400

    Portafolio 3.0

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -1,11 +1,18 @@
 export const trabajos = [
-
+    {
+        "id": "chatapprealtime",
+        "nombre": "Realtime Private Chat App",
+        "url": "https://github.com/pvenegas30/chat-app",
+        "tecnologias": "HTML, SASS, JS, REACT, FIREBASE",
+        "categorias": "Desarrollo de App",
+        "descripcion": "Chat App es una aplicación creada con una funcionalidad a tiempo real para enviar mensajes a usuarios que sean registrados. Para lograrlo, se aplicaron tecnologías como HTML, SASS, JS, REACT y FIREBASE. Chat App is an application created with real-time functionality to send messages to registered users. To achieve this, technologies such as HTML, SASS, JS, REACT and FIREBASE were applied."
+    },
     {
         "id": "weatherapp",
         "nombre": "Weather App Forecast",
         "url": "https://github.com/pvenegas30/weather-app",
         "tecnologias": "HTML, TAILWIND, JS, REACT, LUXON, OPENWEATHER API",
-        "categorias": "Desarrollo web",
+        "categorias": "Desarrollo de App",
         "descripcion": "En esta Weather App se puede consultar el clima de cualquier parte del mundo, ya sea a tiempo real o el pronónstico de días futuros. Para la creación de este proyecto hice uso de tecnologías como HTML, TailWindCSS, JavaScript, React y elementos de Luxon, IconScout, React Toastify y Openweather API. English: In this weather application you can check the weather anywhere in the world, either in real time or the forecast for future days. For the creation of this project I made use of technologies such as HTML, TailWindCSS, JavaScript, React and elements such as Luxon, IconScout, React Toastify and Openweather API."
     },
     {
@@ -34,19 +41,11 @@ export const trabajos = [
         "descripcion": "En informática, la World Wide Web o red informática mundial​ es un sistema que funciona a través de Internet, por el cual se pueden transmitir diversos tipos de datos a través del Protocolo de Transferencia de Hipertextos o HTTP, que son los enlaces de la página web."
     },
     {
-        "id": "moviestime",
-        "nombre": "MoviesTime",
-        "url": "https://pvenegas30.github.io/moviestime",
-        "tecnologias": "HTML, CSS, JS, REACT",
+        "id": "portafolio",
+        "nombre": "Mi Portafolio",
+        "url": "https://github.com/pvenegas30/pedro-venegas-portafolio",
+        "tecnologias": "HTML, CSS, JS, REACT, STYLED COMPONENTS",
         "categorias": "Desarrollo web",
-        "descripcion": "En informática, la World Wide Web o red informática mundial​ es un sistema que funciona a través de Internet, por el cual se pueden transmitir diversos tipos de datos a través del Protocolo de Transferencia de Hipertextos o HTTP, que son los enlaces de la página web."
-    },
-    {
-        "id": "twitter",
-        "nombre": "Twitter",
-        "url": "https://twitter.com",
-        "tecnologias": "HTML, CSS, JS",
-        "categorias": "Desarrollo web",
-        "descripcion": "En informática, la World Wide Web o red informática mundial​ es un sistema que funciona a través de Internet, por el cual se pueden transmitir diversos tipos de datos a través del Protocolo de Transferencia de Hipertextos o HTTP, que son los enlaces de la página web."
+        "descripcion": "¡Hola! Soy un desarrollador web Full Stack, me considero una persona creativa, responsable y con iniciativa. He obtenido mis conocimientos realizando varios cursos profesionales y siempre me encuentro investigando y aprendiendo para mejorar cada vez más mis habilidades."
     }
 ]
